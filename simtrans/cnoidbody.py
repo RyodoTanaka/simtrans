74a065e8cbfebd02ac9ff71e8d8dcc3fe2c1e6a7
RyodoTanaka <groadpg@gmail.com>
2019-08-14 06:28:15 +0900

    Add getter

--- simtrans/cnoidbody.py
+++ simtrans/cnoidbody.py
@@ -50,6 +50,12 @@ class CnoidBodyReader(object):
         self._to_radian = lambda x: x
         self._rel_poses = {}
 
+    def linknamemap(self):
+        return self._linknamemap
+
+    def rel_poses(self):
+        return self._rel_poses
+
     def dict_to_list(self, es):
         if type(es) == dict:
             r = []
